cd375d2f97d9b4e69b8de5cecc1d73a09180e000
JoaoOliveira20 <145066661+JoaoOliveira20@users.noreply.github.com>
2024-05-24 18:54:40 -0300

    ex

---
+++ CursoJs/aula12ex/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+            //BOM DIA!
+            img.src = 'img/manha.jpg'
+            document.body.style.background = '#BB762A'
+        }   else if (hora >= 12 && hora < 18) {
+            //BOA TARDE!
+            img.src = 'img/tarde.jpg'
+            document.body.style.background = '#6B4243'
+        } else {
+            //BOA NOITE!
+            img.src = 'img/noite.jpg'
+            document.body.style.background = '#2A5D94'
+        }
+    }
